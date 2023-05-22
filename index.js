49384384210e9041385095301ffe82c9c3925380
belalhossainS <mdbelal01792@gmail.com>
2023-05-22 09:16:14 +0600

    react helmet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -52,6 +52,19 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    app.get('/services/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const options = {     
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { description: 1, price: 1,available:1, quantity: 1, name: 1,category: 1,picture: 1, seller:1,email:1 }
+      };
+
+      const result = await servicesCollection.findOne(query,options);
+      res.send(result)
+      
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
